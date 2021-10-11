59b495e20ddb395faa29c3153b499f8cc614dc34
Temi-Tegbe <temiloluwat@gmail.com>
2021-10-11 15:28:14 +0100

    first non-descriptive-commit

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,36 @@ namespace eTickets.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+
         }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Actor)
+                .WithMany(a => a.Actor_Movies)
+                .HasForeignKey(am => am.ActorId);
+
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(am => am.Movie)
+                .WithMany(m => m.Actor_Movies)
+                .HasForeignKey(am => am.MovieId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -11,8 +11,14 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Bio")]
         public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
     }
 }
